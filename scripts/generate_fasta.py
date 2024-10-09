069fd355143ba069b52265b192780dc3ca0ff6af
Samantha Chan <117070994+samkchan815@users.noreply.github.com>
2024-10-09 14:05:07 -0700

    generated randomSeq

---
+++ scripts/generate_fasta.py
@@ -0,0 +1,33 @@
+import random
+
+seq = ""
+
+output = "/mnt/c/Users/wammi/OneDrive/Desktop/datasci217/05-first-exam-samkchan815/data/random_sequence.fasta"
+pair = ['A', 'C', 'G', 'T']
+
+with open(output, 'w') as file:
+    count = 0
+    for i in range(0, 1000000):
+        base = random.choice(pair)
+        file.write(base)
+        count += 1
+
+        if count >= 79:
+            file.write('\n')
+            count = 0
+file.close()
+
+# TESTING -----------------
+# with open(output, 'r') as file:
+#     count = 0
+#     while True:
+#         line = file.readline()
+#         if not line:
+#             break
+#         line = line.strip()
+#         count += len(line)
+
+# print(count)
+
+
+# file.close()
